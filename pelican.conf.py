3fa16ec10d1bf1052c6f564b870db369d750e9ea
Ben Bass <benbass@codedstructure.net>
2012-04-11 22:14:57 +0100

    initial commit to github pages

---
+++ pelican.conf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Ben Bass"
+SITENAME = u"Coded Structure"
+SITEURL = 'http://codedstructure.github.com'
+TIMEZONE = 'Europe/London'
+PAGE_DIR = 'pages'
+DEFAULT_LANG='en'
+STATIC_PATHS=['images', 'css', 'js']
+TYPOGRIFY=True
+SOCIAL = (('twitter', 'http://twitter.com/codedstructure'),
+          ('bitbucket', 'http://bitbucket.org/codedstructure'),
+          ('github', 'http://github.com/codedstructure'),
+         )
+DEFAULT_PAGINATION = 5
+DEFAULT_ORPHANS = 2
+DISPLAY_PAGES_ON_MENU = True
+TWITTER_USERNAME = 'codedstructure'
